fdc9ae15179e300b1d15940217af6365ed78519c
Brabus098 <vovan1998888@mail.ru>
2026-02-26 19:08:32 +0300

    [Feature] Add CartService and PaymentService

--- iOS-FakeNFT-Extended/Services/ServicesAssembly.swift
+++ iOS-FakeNFT-Extended/Services/ServicesAssembly.swift
@@ -3,22 +3,54 @@ import Foundation
 @Observable
 @MainActor
 final class ServicesAssembly {
-
+    
     private let networkClient: NetworkClient
     private let nftStorage: NftStorage
-
+    private let paymentStorage: PaymentStorage
+    private let cartStorage: CartStorage
+    
     init(
         networkClient: NetworkClient,
-        nftStorage: NftStorage
+        nftStorage: NftStorage,
+        paymentStorage: PaymentStorage,
+        cartStorage: CartStorage
     ) {
         self.networkClient = networkClient
         self.nftStorage = nftStorage
+        self.paymentStorage = paymentStorage
+        self.cartStorage = cartStorage
     }
-
+    
     var nftService: NftService {
         NftServiceImpl(
             networkClient: networkClient,
             storage: nftStorage
+        )
+    }
+    
+    var paymentService: PaymentServices {
+        PaymentServices(networkClient: networkClient,
+                        storage: paymentStorage)
+    }
+    
+    var cartSevice: CartServiceImpl {
+        CartServiceImpl(networkClient: networkClient, storage: cartStorage)
+    }
+}
+
+// MARK: Preview Helpers
+extension ServicesAssembly {
+    static var preview: ServicesAssembly {
+        let networkClient = DefaultNetworkClient()
+        let nftStorage = NftStorageImpl()
+        let paymentStorage = PaymentStorageImpl()
+        let cartStorage = CartStorageImpl()
+        
+        return ServicesAssembly(
+            networkClient: networkClient,
+            nftStorage: nftStorage,
+            paymentStorage: paymentStorage,
+            cartStorage: cartStorage
         )
     }
 }
