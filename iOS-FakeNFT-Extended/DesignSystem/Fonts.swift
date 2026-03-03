5db52a091e46996b77b36898cb725afd6777d9a6
Sun4chek <boysfromhell@mail.ru>
2026-03-03 12:59:24 +0300

    фикс шрифтов

--- iOS-FakeNFT-Extended/DesignSystem/Fonts.swift
+++ iOS-FakeNFT-Extended/DesignSystem/Fonts.swift
@@ -1,33 +1,22 @@
-import UIKit
 import SwiftUI
 
-extension UIFont {
-    // Ниже приведены примеры шрифтов, настоящие шрифты надо взять из фигмы
-
-    // Headline Fonts
-    static var headline1 = UIFont.systemFont(ofSize: 34, weight: .bold)
-    static var headline2 = UIFont.systemFont(ofSize: 28, weight: .bold)
-    static var headline3 = UIFont.systemFont(ofSize: 22, weight: .bold)
-    static var headline4 = UIFont.systemFont(ofSize: 20, weight: .bold)
-
-    // Body Fonts
-    static var bodyRegular = UIFont.systemFont(ofSize: 17, weight: .regular)
-    static var bodyBold = UIFont.systemFont(ofSize: 17, weight: .bold)
-
-    // Caption Fonts
-    static var caption1 = UIFont.systemFont(ofSize: 15, weight: .regular)
-    static var caption2 = UIFont.systemFont(ofSize: 13, weight: .regular)
-}
-
 extension Font {
-    static let headline1 = Font(UIFont.headline1)
-    static let headline2 = Font(UIFont.headline2)
-    static let headline3 = Font(UIFont.headline3)
-    static let headline4 = Font(UIFont.headline4)
-
-    static let bodyRegular = Font(UIFont.bodyRegular)
-    static let bodyBold = Font(UIFont.bodyBold)
-
-    static let caption1 = Font(UIFont.caption1)
-    static let caption2 = Font(UIFont.caption2)
+    static let headline1 = Font.system(size: 34, weight: .bold)
+    static let headline2 = Font.system(size: 28, weight: .bold)
+    static let headline3 = Font.system(size: 22, weight: .bold)
+    static let headline4 = Font.system(size: 20, weight: .bold)
+    static let headline5 = Font.system(size: 20, weight: .regular)
+    
+    static let bodyRegular = Font.system(size: 17, weight: .regular)
+    static let bodyBold = Font.system(size: 17, weight: .bold)
+    
+    static let caption1 = Font.system(size: 15, weight: .regular)
+    static let caption2 = Font.system(size: 13, weight: .regular)
+    static let caption3 = Font.system(size: 10, weight: .medium)
+    
+    static var body22Bold = Font.system(size: 22, weight: .bold)
+    static var medium10 = Font.system(size: 10, weight: .regular)
+    static var regular13 = Font.system(size: 13, weight: .regular)
+    static var regular15 = Font.system(size: 15, weight: .regular)
 }
+
